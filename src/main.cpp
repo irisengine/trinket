75fd7892fe5b112bfce7e00e5397b7b6f1d2f179
iris dev <iris.engine.dev@gmail.com>
2021-12-28 21:13:17 +0000

    Add iris

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//         Distributed under the Boost Software License, Version 1.0.         //
+//            (See accompanying file LICENSE or copy at                       //
+//                 https://www.boost.org/LICENSE_1_0.txt)                     //
+////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 
-int main()
+#include "iris/core/start.h"
+
+void go(int, char **)
 {
     std::cout << "hello trinket" << std::endl;
-
-    return 0;
 }
 
+int main(int argc, char **argv)
+{
+    iris::start(argc, argv, go);
+    return 0;
+}
